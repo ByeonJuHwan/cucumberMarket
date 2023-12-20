c7d6c7e4bd8cef67048d09dd23a4b991f862413e
juhwan <asdcz@naver.com>
2023-12-20 23:07:49 +0900

    사용하지 않는 메서드 삭제

--- src/main/java/com/sohwakmo/cucumbermarket/repository/PostRepository.java
+++ src/main/java/com/sohwakmo/cucumbermarket/repository/PostRepository.java
@@ -11,8 +11,5 @@ public interface PostRepository extends JpaRepository<Post,Integer>, PostReposit
     //TODO QueryDsl 로 처리
     List<Post> findByTitleIgnoreCaseContainingOrContentIgnoreCaseContainingOrMemberNicknameIgnoreCaseContainingOrderByPostNoDesc(String title, String content,String nickname);
     List<Post> findByTitleIgnoreCaseContainingOrContentIgnoreCaseContainingOrMemberNicknameIgnoreCaseContainingAndMemberAddressIgnoreCaseContainingOrderByPostNoDesc(String title, String content,String nickname,String address);
-
-    Post findByImageName01(String imageSrc);
-    Post findByImageName02(String imageSrc);
     List<Post> findByOrderByClickCountDescPostNoDesc();
 }
